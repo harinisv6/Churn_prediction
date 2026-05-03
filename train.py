e80812ecfa850843cd9c3c1058c8bcb7bc882bea
HARINI S <harinisv6@gmail.com>
2026-05-03 13:48:16 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression    
 from sklearn.metrics import (
     accuracy_score,
     confusion_matrix,
