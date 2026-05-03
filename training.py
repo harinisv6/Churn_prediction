1ff944756ab5386a5017b0d6f8ccd4105fd817b4
HARINI S <harinisv6@gmail.com>
2026-05-03 15:45:57 +0530

    Update training.py

--- training.py
+++ training.py
@@ -15,7 +15,7 @@ from sklearn.preprocessing import StandardScaler
 # ==========================================
 # 1. LOAD DATA
 # ==========================================
-df = pd.read_excel("data/churn.xlsx")
+df = pd.read_excel("data/Telco_customer_churn.xlsx")
 
 
 # ==========================================
